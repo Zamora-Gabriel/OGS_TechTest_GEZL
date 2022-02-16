619a71e41539810f935c52eb8b08f6e8ff8597e1
Gabriel Zamora <pg20gabriel@vfs.com>
2022-02-15 19:17:00 -0600

    UI advances on the roulette

---
+++ OGS_TechTest/Assets/Scripts/Enums.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SELECTEDOPTION
+{
+    FUSION,
+    DISCOVER,
+    FORGE,
+    MIX
+}
+
+public enum SELECTEDFAMILY
+{
+    RED,
+    YELLOW,
+    GREEN,
+    CYAN,
+    BLUE,
+    PURPLE
+}
